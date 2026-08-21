49033709e1c5a310fc53143280243f66423d1c75
Mohit Gupta <mohitgupta@MacBookPro.lan>
2026-08-21 23:11:23 +0530

    nevbar adding complete

--- expense-tracker/src/pages/home/HomePage.jsx
+++ expense-tracker/src/pages/home/HomePage.jsx
@@ -1,15 +1,13 @@
 import { Sidebar } from "../../components/Sidebar/Sidebar";
+import "./HomePage.css";
 
 export function HomePage() {
   return (
     <>
         <Sidebar />
-        <div className="flex">
-        <Sidebar />
-        <div className="flex-1 p-4">
-            <h1 className="text-2xl font-bold mb-4">Welcome to the Expense Tracker</h1>
+        <div className="homepage-content">
+            <h1 >Welcome to the Expense Tracker</h1>
             <p>Track your expenses and manage your budget effectively.</p>
-        </div>
         </div>
     </>
   );
